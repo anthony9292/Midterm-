4ebceec6c11de798550bfcfc7a98f2e02a76642c
Anthony Ndegwa <anthon.n24@gmail.com>
2021-03-18 21:23:18 -0400

    update

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var game = {
+state: {
+
+startButton: $("#start-Button"), 
+
+gameContainer: $("#game"), 
+scoreNumber: $("#score-number"),
+questionsView: $(".questions"),
+gameEndView: $("#game-end"),
+gameEndViewTimesUp: $("#game-end-times-up"),
+gameEndText: $("#game-end-text"),
+timeOutText: $("#time-out-text"),
+gauge: $("#gauge"),
+questions: $(".question"),
+answers: $(".answer"),
+timer: $("#timer"),
+indicators: $(".indicator"),
+numberOfQuestions: $(".question").length,
+questionsAnswered: 0,
+correctAnswers: 0
+  },
+
+
+
+
+}
